dae41b23e70f16d8a25b24d5a26351e8e83470c2
Ishan Pradhan <ishanpradhan72@gmail.com>
2024-11-27 17:59:40 +0545

    blog modeling

---
+++ Backend/src/models/Blog.models.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const blogSchema = new Mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  image: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+export const Blog = mongoose.model("Blog", blogSchema);
