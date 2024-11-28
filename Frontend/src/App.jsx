f1606ec227ed0b727ecfae1e4d447890e9bd2ace
Ishan Pradhan <ishanpradhan72@gmail.com>
2024-11-28 17:03:17 +0545

    removed error boundary

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,26 @@
-import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateBlog from "./pages/CreateBlog";
-import ErrorBoundary from "./components/ErrorBoundry";
 
 import LoginPage from "./pages/Auth/LoginPage";
 import SingleBlog from "./pages/SingleBlog";
 import SignupPage from "./pages/Auth/SignupPage";
 
-const App = ()=>{
+const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route index element={<Home/>}/> 
-      <Route path="/Home" element={<Home/>}> </Route>
-      <Route path="/CreateBlog" element={<CreateBlog/>}></Route>
-      <Route path="/SingleBlog" element={<SingleBlog/>}></Route>
-      <Route path="/login" element={<LoginPage/>}></Route>
-      <Route path="/signup" element={<SignupPage/>}></Route>
-  
-    </Routes>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/Home" element={<Home />}>
+          {" "}
+        </Route>
+        <Route path="/CreateBlog" element={<CreateBlog />}></Route>
+        <Route path="/SingleBlog" element={<SingleBlog />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
